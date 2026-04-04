5b55c0a8e70f9095a053b9292b49d9d2ee83fea1
Tom Seidel <saphirkobayashi@gmail.com>
2026-04-04 16:39:02 +0200

    Updated monitor_report creation
    Adjusted code to work with new folder structure and both frameworks

--- biodcase2026_tiny_ml_pytorch.py
+++ biodcase2026_tiny_ml_pytorch.py
@@ -10,6 +10,7 @@ from pipeline_pytorch.model_training import pytorch_model_taining
 from embedded_code_generation import run_compile_embedded_src_code, run_create_target_embedded_src_code, run_deploy_embedded_compiled_code
 from model_evaluation import model_evaluation
 from model_quantization import model_quantization
+from biodcase_tiny.embedded.esp_monitor_parser import finalize_monitor_report
 
 if __name__ == '__main__':
   """
@@ -65,3 +66,6 @@ if __name__ == '__main__':
 
   # deploy
   run_deploy_embedded_compiled_code(cfg)
+
+  # finalize the monitor report yaml
+  finalize_monitor_report("pytorch")
